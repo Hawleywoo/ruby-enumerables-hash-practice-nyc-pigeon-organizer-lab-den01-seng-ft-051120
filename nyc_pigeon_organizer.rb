d062eb14c1eecccf8d93b67e5757aa75ae38bdea
Andrew Hawley <ahawley785@gmail.com>
2020-05-06 01:01:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,45 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |color_gender_lives, value|
+    puts color_gender_lives
+    puts value
     value.each do |stats, all_names|
+      puts stats
+      puts all_names
       all_names.each do |name|
+
         if pigeon_list[name] == nil
-          pigeon_list[name] = {}
+           pigeon_list[name] = {}
         end
         if pigeon_list[name][color_gender_lives] == nil
           pigeon_list[name][color_gender_lives] = []
         end
         pigeon_list[name][color_gender_lives].push(stats.to_s)
+
       end
     end
   end
   pigeon_list
 end
+
+pigeon_data = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+    :blue => ["Luca"],
+    :green => ['Luca']
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
+
+
+nyc_pigeon_organizer(pigeon_data)
